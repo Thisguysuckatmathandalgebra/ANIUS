dd303c3058ecfb04dc1aa6709227501e01926814
Thisguysuckatmathandalgebra <166210627+Thisguysuckatmathandalgebra@users.noreply.github.com>
2024-08-30 18:39:08 +0800

    Update and rename public.html to server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let onlineUsers = 0;
+
+// Serve static files (make sure public.html and style.css are in the same directory as server.js)
+app.use(express.static(path.join(__dirname)));
+
+// Handle a GET request to the root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  onlineUsers++;
+  io.emit('user count', onlineUsers);
+
+  socket.on('disconnect', () => {
+    onlineUsers--;
+    io.emit('user count', onlineUsers);
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
